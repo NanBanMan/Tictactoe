aa53fb344aac376645adf9d7702365d629ceed24
NanBanMan <104819291+NanBanMan@users.noreply.github.com>
2024-09-24 11:32:02 +0100

    Add main program and functions files via upload

---
+++ tictactoe.py
@@ -0,0 +1,129 @@
+import random
+import tictactoefunctions as func
+
+actual_board_state = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+win_state = False
+
+print("Hi! Let's play Tic Tac Toe!")
+
+player_mark = input("Would you like to be X or O?").upper()
+
+while player_mark not in ['X', 'x', 'O', 'o']:
+    player_mark = input("I'm sorry, please choose X or O:")
+
+if player_mark == 'O':
+    computer_mark = 'X'
+    print(f"Great! You are {player_mark}s, I'll be {computer_mark}s!")
+else:
+    computer_mark = 'O'
+    print(f"Great! You are {player_mark}s, I'll be {computer_mark}s!")
+
+
+print("I'll randomly choose who goes first...")
+
+coinflip = random.randint(0,1)
+
+if coinflip == 0:
+    player_turn = True
+    print("You get to go first")
+else:
+    player_turn = False
+    print("I'll go first!")
+
+func.print_board(actual_board_state)
+
+turn_count = 0
+
+while win_state == False:
+
+    if turn_count == 9:
+        print("Game Over")
+        win_state = True
+        break
+
+    if player_turn == True:
+        turn_count +=1
+        while True:
+            try:    
+                choice = int(input("Which square do you choose? "))
+                break
+            except:
+                print("Please choose an empty square number!")
+        while choice not in range(1,10) or actual_board_state[choice-1] == 'X' or actual_board_state[choice-1] =='0':
+            choice = input("Please choose an empty square number:")
+
+        choice = int(choice)
+        
+        print("Good move!")
+        actual_board_state[choice -1] = player_mark
+        func.print_board(actual_board_state)
+        win_check = func.check_player_win(actual_board_state, player_mark)
+        
+        if win_check is True:
+            print("You win!")
+            win_state = True
+        else:
+            player_turn = False
+
+    if turn_count == 9:
+        print("Game Over")
+        win_state = True
+        break
+
+    if player_turn == False:
+        turn_count +=1
+        for square in actual_board_state:
+            if square == 'O' or square == 'X':
+                pass
+            else:                
+                hypo_board_state = actual_board_state.copy()
+                hypo_board_state[int(square) - 1] = computer_mark
+                if (func.check_player_win(hypo_board_state, computer_mark) is True):
+                    print(f"My turn! I want square number {actual_board_state[int(square)-1]}!")
+                    actual_board_state[int(square) - 1] = computer_mark
+                    func.print_board(actual_board_state)
+                    print("I win!")
+                    player_turn = True
+                    win_state = True
+                    break
+    if player_turn == False:   
+        for square in actual_board_state:
+            if square == 'O' or square == 'X':
+                pass
+            else:
+                    hypo_board_state = actual_board_state.copy()
+                    hypo_board_state[int(square) - 1] = player_mark
+                
+                    if (func.check_player_win(hypo_board_state, player_mark) is True):
+                        print(f"My turn! I'll choose square number {actual_board_state[int(square)-1]}!")
+                        actual_board_state[int(square) - 1] = computer_mark
+                        func.print_board(actual_board_state)
+                        print("Your turn!")
+                        player_turn = True
+                
+                        break
+    
+    if player_turn == False:
+            path_scores = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+            for square in actual_board_state:
+                if square == 'O' or square == 'X':
+                    pass
+                else:
+                    path_scores[square] = func.assess_moves(actual_board_state, computer_mark, player_mark, player_turn, 0, square)
+            
+            next_move = 0
+            next_move_value = -1000
+
+
+            for square, value in path_scores.items():
+                if value > next_move_value:
+                    next_move = square
+            
+            print(f"I'll choose square number {next_move}!")
+
+            actual_board_state[int(next_move)-1] = computer_mark
+            func.print_board(actual_board_state)
+
+            player_turn = True
+           
+
---
+++ tictactoefunctions.py
@@ -0,0 +1,86 @@
+
+def print_board(actual_board_state):
+    
+    print(f"{actual_board_state[0]}|{actual_board_state[1]}|{actual_board_state[2]}\n-----\n{actual_board_state[3]}|{actual_board_state[4]}|{actual_board_state[5]}\n-----\n{actual_board_state[6]}|{actual_board_state[7]}|{actual_board_state[8]}")
+
+'''def check_win(abs, mark):
+
+    #check for winning move
+
+    
+    if (abs[1] == mark and abs[2] == mark) or (abs[3] == mark and abs[6] ==mark) or (abs[4] == mark and abs[8] == mark):
+        return 0
+    if (abs[0] == mark and abs[2] == mark) or (abs[4] == mark and abs[7] == mark):
+        return 1
+    if (abs[0] == mark and abs[1] == mark) or (abs[5] == mark and abs[5] ==mark) or (abs[4] == mark and abs[6] == mark):
+        return 2    
+    if (abs[4] == mark and abs[5] == mark) or (abs[0] == mark and abs[6] == mark):
+        return 3
+    if (abs[3] == mark and abs[5] == mark) or (abs[1] == mark and abs[7] ==mark) or (abs[0] == mark and abs[8] == mark) or (abs[2] == mark and abs[6] == mark):
+        return 4
+    if (abs[3] == mark and abs[4] == mark) or (abs[2] == mark and abs[8] == mark):
+        return 5    
+    if (abs[7] == mark and abs[8] == mark) or (abs[0] == mark and abs[3] ==mark) or (abs[2] == mark and abs[4] == mark):
+        return 6
+    if (abs[6] == mark and abs[8] == mark) or (abs[1] == mark and abs[4] == mark):
+        return 7
+    if (abs[6] == mark and abs[7] == mark) or (abs[0] == mark and abs[4] ==mark) or (abs[2] == mark and abs[5] == mark):
+        return 8
+    else:
+        return False'''
+    
+
+def check_player_win(abs, mark):
+    
+    if (abs[0] == mark and abs[1] == mark and abs[2] == mark):
+        return True
+    if (abs[3] == mark and abs[4] == mark and abs[5] == mark):
+        return True
+    if (abs[6] == mark and abs[7] == mark and abs[8] == mark):
+        return True   
+    if (abs[0] == mark and abs[3] == mark and abs[6] == mark):
+        return True
+    if (abs[1] == mark and abs[4] == mark and abs[7] == mark):
+        return True
+    if (abs[2] == mark and abs[5] == mark and abs[8] == mark):
+        return True  
+    if (abs[0] == mark and abs[4] == mark and abs[8] == mark):
+        return True
+    if (abs[2] == mark and abs[4] == mark and abs[6] == mark):
+        return True
+    else:
+        return False
+
+
+
+
+    
+
+def assess_moves(actual_board_state, turn_player_mark, opponent_mark, player_turn, path_score, square_ref):
+
+#loop through empty squares, try the next move there on a new copy of the board state
+    
+    hypo_board_state = actual_board_state.copy()
+    hypo_board_state[int(square_ref) - 1] = turn_player_mark
+    if (check_player_win(hypo_board_state, turn_player_mark) is True) and (player_turn == False):
+        path_score += 1
+    if (check_player_win(hypo_board_state, turn_player_mark) is True) and (player_turn ==True):
+        path_score -= 1
+    else:
+        if turn_player_mark == 'X':
+            turn_player_mark = 'O'
+            opponent_mark = 'X'
+        if turn_player_mark == 'O':
+            turn_player_mark ='X'
+            opponent_mark ='0'
+        player_turn = not player_turn
+        for square in hypo_board_state:
+            if square == 'O' or square == 'X':
+                pass
+            else:
+                path_score = assess_moves(hypo_board_state, turn_player_mark, opponent_mark, player_turn, path_score, square)
+    return path_score
+    
+            
+    
+
